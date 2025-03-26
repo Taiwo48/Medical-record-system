7c13491ae95bf0c200d57e6e08957f4e3aeee803
TosinAdewuyi19 <adewuyitosin19@gmail.com>
2025-03-26 15:40:49 +0100

    Tosin

---
+++ src/main/java/Repositories/PatientRepository.java
@@ -0,0 +1,7 @@
+package Repositories;
+
+public interface PatientRepository {
+    void save(patient patient);
+    patient save
+
+}
---
+++ src/main/java/User.java
@@ -0,0 +1,57 @@
+public class User {
+    private String name;
+    private int age;
+    private String gender;
+    private String id;
+    private String password;
+
+
+
+    public void user(String name, String gender, int age, String id, String password ) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.id = id;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
